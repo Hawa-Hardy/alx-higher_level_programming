6b96eeb8d55ab4d8133a8c8d6b50187760578b4b
Hawa-Hardy <hawahardy@gmail.com>
2024-01-26 14:32:25 +0000

    Files

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if not my_list:
-        return my_list
-    return [val if val != search else replace for val in my_list]
+    return [valu if valu != search else replace for valu in my_list]
