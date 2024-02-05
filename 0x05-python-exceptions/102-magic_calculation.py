7a241b2eee4d499725191bf3b7d2b6f378214780
Hawa-Hardy <hawahardy@gmail.com>
2024-02-05 19:28:15 +0000

    extra new functions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    sum = 0
+    result = 0
     for idx in range(1, 3):
         try:
             if (idx > a):
                 raise Exception("Too far")
             else:
-                sum += (a ** b) / idx
+                result += (a ** b) / idx
         except:
-            sum = b + a
+            result = b + a
             break
-    return (sum)
+    return (result)
