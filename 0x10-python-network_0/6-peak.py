7b784c46527ac7c3481680a41a22ed5882e9f985
Hawa-Hardy <hawahardy@gmail.com>
2024-05-12 17:06:07 +0000

    more efficent

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 """Defines a peak-finding algorithm."""
 
-
 def find_peak(list_of_integers):
     """Return a peak in a list of unsorted integers."""
-    if list_of_integers == []:
+    def peak_helper(lo, hi):
+        if lo == hi:
+            return list_of_integers[lo]
+        mid = (lo + hi) // 2
+        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) and \
+           (mid == len(list_of_integers) - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
+            return list_of_integers[mid]
+        elif mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            return peak_helper(lo, mid - 1)
+        else:
+            return peak_helper(mid + 1, hi)
+
+    if not list_of_integers:
         return None
-
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
-
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    return peak_helper(0, len(list_of_integers) - 1)
